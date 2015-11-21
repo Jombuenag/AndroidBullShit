122caccbb72feb875a11c644419473d40c0d36b7
Jorge <Takumakun@gmail.com>
2015-11-21 04:30:22 +0100

    Todo Ok. Los datos pasan correctamente.

--- app/src/main/java/com/example/takuma/pasodeparametros/Pantalla1.java
+++ app/src/main/java/com/example/takuma/pasodeparametros/Pantalla1.java
@@ -1,7 +1,10 @@
 package com.example.takuma.pasodeparametros;
 
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -9,28 +12,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
-
 
 public class Pantalla1 extends AppCompatActivity {
 
     Button enviarInfo;
-    EditText textoNombre;
+    TextView textoNombre;
     RadioGroup radioGroup;
     TextView datosRecibidos;
+    Switch carnetConducir;
+    RatingBar ratingBar;
+    SeekBar seekBar;
+
     final int PASOAPANTALLA_2=1;
-    String mensaje ="";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
+
         super.onCreate(savedInstanceState);
+        setTitle("PasoDeParametros");
         setContentView(R.layout.activity_pantalla1);
 
-        enviarInfo = (Button) findViewById(R.id.enviarInfo);
+
         textoNombre = (EditText) findViewById(R.id.textoNombre);
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
-        datosRecibidos = (TextView)findViewById(R.id.textoNombre);
+        carnetConducir = (Switch)findViewById(R.id.switchConducir);
+        ratingBar = (RatingBar)findViewById(R.id.ratingBar);
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
+        enviarInfo = (Button) findViewById(R.id.enviarInfo);
 
         enviarInfo.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,19 +51,42 @@ public class Pantalla1 extends AppCompatActivity {
                 if (datosCorrectos()){
                     Intent i = new Intent(getApplicationContext(),Pantalla2.class);
                     Bundle b = new Bundle();
-                    b.putString("Nombre",textoNombre.getText().toString()); //AÑADE EL PARAMETRO NOMBRE
+                    b.putString("Nombre", textoNombre.getText().toString()); //AÑADE EL PARAMETRO NOMBRE
+                    b.putString("Resultado", String.valueOf(ratingBar.getRating()));//AÑADE LA PUNTUACION
+                    b.putString("SeekResultado", String.valueOf(seekBar.getProgress()));
                     switch (radioGroup.getCheckedRadioButtonId()) {
-                        case R.id.rb1:
+                        case R.id.rbHombre:
                             b.putString("Sexo","Hombre");
                             break;
-                        case R.id.rb2:
+                        case R.id.rbMujer:
                             b.putString("Sexo","Mujer");
                             break;
                         default:
                             b.putString("Sexo","Indefinido");
                     }
+                    if(carnetConducir.isEnabled()){
+                            b.putString("Permiso","tienes el carnet");
+                    }else{
+                            b.putString("Permiso","no tienes el carnet");
+                    }
+
                     i.putExtras(b); // AÑADE EL OBJETO BUNDLE AL INTENT
-                    startActivityForResult(i,PASOAPANTALLA_2); //LLAMA A LA SUBACTIVIDAD Y AL INTENT QUE CONTIENE EL BUNDLE
+                    startActivityForResult(i, PASOAPANTALLA_2); //LLAMA A LA SUBACTIVIDAD Y AL INTENT QUE CONTIENE EL BUNDLE
+
+                    NotificationCompat.Builder mensaje = new NotificationCompat.Builder(Pantalla1.this);
+                        if(radioGroup.getCheckedRadioButtonId() == R.id.rbHombre) {
+                            mensaje.setContentTitle("Hombre");
+                            mensaje.setContentText("Has elegido chico ");
+                            mensaje.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.vaultboy));
+                            mensaje.setSmallIcon(R.drawable.vaultboy);
+                        }else {
+                            mensaje.setContentTitle("Mujer");
+                            mensaje.setContentText("Has elegido chica");
+                            mensaje.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.chibicat));
+                            mensaje.setSmallIcon(R.drawable.chibicat);
+                        }
+                        NotificationManager manager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+                        manager.notify(1, mensaje.build()); //ENVIA LA NOTIFICACIÓN CON ID:1
                 } else {
                     Toast.makeText(getApplicationContext(), "Datos incorrectos, falta rellenar algún campo", Toast.LENGTH_LONG).show();
                 }
@@ -71,25 +107,27 @@ public class Pantalla1 extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent i){
-        super.onActivityResult(requestCode, resultCode, i);
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
         switch (requestCode){
             case PASOAPANTALLA_2:
-                gestionaSubActivity(resultCode, i);
+                gestionaSubActivity(resultCode, data);
                 break;
         }
     }
 
-    public void gestionaSubActivity(int resutlCode, Intent i) {
-        if (resutlCode == RESULT_OK) {
-            Bundle b = i.getExtras();
+    public void gestionaSubActivity(int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            String mensaje="";
+            Bundle b = data.getExtras();
             int edad = b.getInt("Edad");
-            //if (edad < 18) mensaje = new String("¡Yogurin!");
-            //if ((edad >= 18) && (edad < 25)) mensaje = new String("En la flor de la vida");
-            //if (edad > 25) mensaje = new String("Ya van pesando las cosas de la edad");
-            //AHORA PASAMOS POR PANTALLA
+            if (edad < 18) mensaje = new String("¡Yogurin!");
+            if ((edad >= 18) && (edad < 25)) mensaje = new String("En la flor de la vida");
+            if (edad > 25) mensaje = new String("Ya van pesando las cosas de la edad");
+            //AHORA PASAMOS POR PANTALLA LOS DATOS DE LA ACTIVITY 2
+
             datosRecibidos.setText(mensaje.toString());
-            desactivaComponentes();
+            //desactivaComponentes();
         } else {
             Toast.makeText(getApplicationContext(), "Error en la SubActividad 1", Toast.LENGTH_LONG).show();
         }
@@ -98,16 +136,11 @@ public class Pantalla1 extends AppCompatActivity {
     public void desactivaComponentes(){
         textoNombre.setEnabled(false); //DESACTIVA EL CAMPO NOMBRE
         radioGroup.setEnabled(false); //LO MISMO CON EL RG PERO HAY QUE IR UNO A UNO
+        //ME GUSTA USAR LA LETRA J PARA SEPARARLA DE LA I, LA CUAL USARAEMOS PARA EL INTENT
         for(int j=0;j<radioGroup.getChildCount();j++){
             radioGroup.getChildAt(j).setEnabled(false);
         }
         enviarInfo.setEnabled(false); //POR ULTIMO DESACTIVAMOS EL BOTON;
-    }
-
-
-    public void pasaDePantalla(View v){
-        Intent i = new Intent(getApplicationContext(),Pantalla2.class);
-        startActivity(i);
     }
 
     @Override
--- app/src/main/java/com/example/takuma/pasodeparametros/Pantalla2.java
+++ app/src/main/java/com/example/takuma/pasodeparametros/Pantalla2.java
@@ -8,45 +8,62 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class Pantalla2 extends AppCompatActivity {
     String nombre; //AQUI GUARDAMOS LOS DATOS DE PANTALLA1
     String sexo; //AQUI GUARDAMOS LOS DATOS DE PANTALLA1
 
     String mensaje;
-    Button finalizar;
-    EditText edad;
-    EditText nombrePantalla2;
-    final int PASOPANTALLA_1=1;
+    Button enviarDatos;
+    TextView nombrePantalla2;
+    String rating = "";
+    String carnet = "";
+    String seekRating = "";
+
+    final int PASOPANTALLA_1 = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla2);
-        finalizar = (Button) findViewById(R.id.botonFinalizar);
-        edad = (EditText) findViewById(R.id.textoEdad);
-        nombrePantalla2 = (EditText) findViewById(R.id.nombrePantalla2);
+        //ENLAZAMOS EL TEXTVIEW DEL XML CON EL OBJETO TEXTVIEW
+        enviarDatos = (Button) findViewById(R.id.botonEnviar);
+        nombrePantalla2 = (TextView) findViewById(R.id.nombrePantalla2);
+        //rating = (String)findViewById(R.id.rating);
 
         //VAMOS A PILLAR LOS DATOS QUE NOS DA EL BUNDLE DE LA ACTIVITY 1
         Bundle b = getIntent().getExtras();
         if (b != null) {
             nombre = b.getString("Nombre");
+            rating = b.getString("Resultado");
+            seekRating = b.getString("SeekResultado");
+            carnet = b.getString("Permiso");
+
+            //rating.setText(b.getString("Resultado"));
             sexo = b.getString("Sexo");
             if(sexo.compareTo("Hombre") == 0) {
-                mensaje = nombre;
+                mensaje = ("Lord " + nombre + " ¿Esto es lo que nos has contado? " +
+                        "\n" + "y nos has puesto una puntuación de  : " + rating + " estrellas " +
+                        "\n" + "o " + seekRating + "/100. Menos mal que " + carnet);
             } else {
-                mensaje = nombre;
+                mensaje = ("Lady " + nombre + " ¿Esto es lo que nos has contado? " +
+                        "\n" + "y nos has puesto una puntuación de  : " + rating + " estrellas " +
+                        "\n" + "o " + seekRating + "/100. Menos mal que " + carnet);
             }
             nombrePantalla2.setText(mensaje.toString());
         } else {
             nombrePantalla2.setText("No he recibido ningún dato");
         }
-        finalizar.setOnClickListener(new View.OnClickListener() {
+        //VAMOS A PONERLE UN LISTENER AL BOTON DE VOLVER
+        enviarDatos.setOnClickListener(new View.OnClickListener() {
+            Intent i = new Intent(getApplicationContext(),Pantalla1.class);
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),Pantalla1.class);
-                startActivity(i);
+                startActivityForResult(i, PASOPANTALLA_1);
             }
         });
     }
@@ -59,12 +76,9 @@ public class Pantalla2 extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
